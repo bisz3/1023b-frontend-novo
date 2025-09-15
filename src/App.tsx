45643fd214f3fe63f1a687394a9c5df998fc895e
bisz3 <lorrainy.peralta@estudante.ifms.edu.br>
2025-09-15 11:28:50 -0300

    atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+
+type ProdutoType = {
+  id: number
+  nome: string
+  preco: number
+  descricao: string
+  urlfoto: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+
+  useEffect(() => {
+    fetch('/api/produtos')
+      .then((response) => response.json())
+      .then((data) => setProdutos(data))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>HAHAHAHAHA</div>
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <h2>{produto.nome}</h2>
+          <p>{produto.descricao}</p>
+          <p>R$ {produto.preco.toFixed(2)}</p>
+          <img src={produto.urlfoto} alt={produto.nome} width="200" />
+        </div>
+      ))}
     </>
   )
 }
